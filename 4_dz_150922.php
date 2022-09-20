aab7e75deb599a8c7c4dd602b056f94dfd86d5c3
SlaShil <108797408+SlaShil@users.noreply.github.com>
2022-09-20 14:29:48 +0300

    Add files via upload

--- 4_dz_150922.php
+++ 4_dz_150922.php
@@ -2,7 +2,23 @@
 /* Создать функцию принимающую массив произвольной вложенности
 и определяющий любой элемент номер которого передан параметром
 во всех вложенных массивах */
-
+$arr = [15,[10,20,[1,2,3,4,5],30,40,50],25,35,45,55];
+function arrayParse(array $arr, int $externalKey): array
+{
+    $result = [];
+    foreach ($arr as $key => $value) {
+        if (is_array($value)) {
+            $result = array_merge($result, arrayParse($value, $externalKey));
+        } else {
+            if ($key === $externalKey) {
+                $result[] = $value;
+            }
+        }
+    }
+    return $result;
+}
+var_dump(arrayParse($arr, 4));
+echo '<br>';
 
 /* Создать функцию которая считает все буквы b в переданной строке,
 в случае если передается не строка функция должна возвращать false */
@@ -13,20 +29,39 @@ function count_b($str) {
              {   echo 'false';
         }
 }
-count_b('Mary born in Berlin in november');
+count_b('Bob was born in Berlin');
 echo '<br>';
 count_b(22.15);
 echo '<br>';
 
+// Вариант 2
+$testStr = 'Bob was born in Berlin';
+$subStr = 'b';
+
+function searchSubStr($needle, $haystack): int|bool
+{
+    if (is_string($needle)) {
+        return substr_count($needle, $haystack);
+    } else {
+        return false;
+    }
+}
+var_dump(searchSubStr($testStr, $subStr));
+echo '<br>';
+
 /* Создать функцию которая считает сумму значений всех элементов массива произвольной глубины */
-function summary($arr) {
+$massive = [15,[10,20,[1,2,3,4,5],30,40,50],25,35,45,55];
+function summary($massive): int
+{
     $summa=0;
-    foreach ($arr as $value) {
-        $summa += $value;
+    foreach ($massive as $value) {
+        if(is_array($value)){
+            $summa += summary($value);
+        }
+        else $summa += $value;
     }
     return $summa;
 }
- $massive=array(30,20,10);
 echo summary($massive);
 echo '<br>';
 
